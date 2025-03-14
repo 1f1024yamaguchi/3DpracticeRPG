77d8b34b81101f6de4a2f70add5828d2418b8ea4
1f1024yamaguchi <s1f102401472@iniad.org>
2025-03-14 19:17:18 +0900

    霧調整

--- Assets/3DRPG/Scripts/RoundLight.cs
+++ Assets/3DRPG/Scripts/RoundLight.cs
@@ -5,7 +5,7 @@ public class RoundLight : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Update()
     {
-        transform.Rotate(new Vector3(0,-6) * Time.deltaTime);
+        transform.Rotate(new Vector3(0,-3) * Time.deltaTime);
     }
 
 
