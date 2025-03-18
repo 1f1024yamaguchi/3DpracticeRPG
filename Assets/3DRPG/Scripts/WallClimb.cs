e370235ad78a3e5b813c394d9b5a4bc8425c5de6
1f1024yamaguchi <s1f102401472@iniad.org>
2025-03-18 18:15:23 +0900

    壁登り実装開始

--- Assets/3DRPG/Scripts/WallClimb.cs
+++ Assets/3DRPG/Scripts/WallClimb.cs
@@ -2,31 +2,76 @@ using UnityEngine;
 
 public class WallClimb : MonoBehaviour
 {
-    public float climbSpeed =3f; //登る速度
-    private Rigidbody rb;
-    private bool isClimbing = false;
+    public float wallCheckOffset =1.0f;
+    public float upperWallCheckOffset = 2.0f;
+    public float wallCheckDistance = 1.0f;
+
+    private bool isForwardWall;
+    private bool isUpperWall;
+    private bool isGrab;
+    private bool isClimb;
+
+    private Vector3 climbOldPos;
+    private Vector3 climbPos;
+
+    private Animator anim;
+    private CharacterController controller;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isClimbing && Input.GetKey(KeyCode.W))
+        Ray wallCheckRay = new Ray(transform.position + Vector3.up * wallCheckOffset, transform.forward);
+        Ray upperCheckRay = new Ray(transform.position + Vector3.up * upperWallCheckOffset, transform.forward);
+        isForwardWall = Physics.Raycast(wallCheckRay, wallCheckDistance);
+        isUpperWall = Physics.Raycast(upperCheckRay, wallCheckDistance);
+
+        if (isForwardWall && !isUpperWall && Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x , climbSpeed, rb.linearVelocity.z);
+            isGrab = true;
+        }
+
+        //壁つかまり中の処理
+        if (isGrab && !isForwardWall)
+        {
+            controller.enabled = false; //移動を無効化
+            if (Input.GetKey(KeyCode.W))//全身入力でよじ登り開始
+            {
+                climbOldPos = transform.position;
+                climbPos = transform.position + transform.forward * 2.0f + Vector3.up * 2.5f;
+                isGrab = false;
+                isClimb = true;
+                anim.SetTrigger("Clumb");
+            }
+        }
+
+        //よじ登り処理
+        if (isClimb)
+        {
+            float f = anim.GetFloat("ClimbProgress"); //アニメーションの進行度
+            float x = Mathf.Lerp(climbOldPos.x , climbPos.x , Ease(f));
+            float z = Mathf.Lerp(climbOldPos.z, climbPos.z, Ease(f));
+            float y = Mathf.Lerp(climbOldPos.y, climbPos.y, f);
+
+            transform.position = new Vector3(x,y,z);
+
+            if (f >= 0.8f)
+            {
+                isClimb = false;
+                controller.enabled = true; //移動を開始
+            }
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    float Ease(float x)
     {
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            isClimbing = false;
-            rb.useGravity = true; //
-        }
+        return x * x * x;
     }
 }
