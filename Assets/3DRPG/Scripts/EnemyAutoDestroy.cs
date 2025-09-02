df4a959751e717823ed884726695faf3ad2e9203
1f1024yamaguchi <s1f102401472@iniad.org>
2025-09-03 00:26:39 +0900

    敵を自動で消すプログラム

--- Assets/3DRPG/Scripts/EnemyAutoDestroy.cs
+++ Assets/3DRPG/Scripts/EnemyAutoDestroy.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class EnemyAutoDestroy : MonoBehaviour
 {
+    [SerializeField] private float destroyDistance = 30f;
+    private Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform; //プレイヤーをタグで取得
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(player == null) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if(distance > destroyDistance)
+        {
+            Destroy(gameObject); //一定距離以上なら消す
+        }
         
     }
 }
